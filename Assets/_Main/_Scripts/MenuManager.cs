ce4425ead81c71f3c4724fa731eee923c07a379d
geropython <72422102+geropython@users.noreply.github.com>
2023-06-03 10:54:02 -0300

    Fadeout game

--- Assets/_Main/_Scripts/MenuManager.cs
+++ Assets/_Main/_Scripts/MenuManager.cs
@@ -3,26 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public Animator anim;
-    public static bool fade_In = false;
-    public GameObject Fade_In;
-
-    public void FadeInAnimation()
-    {
-        Fade_In.SetActive(true);
-        anim.Play("Fade_In");
-
-        StartGame();
-    }
-
-    public void FadeOut()
-    {
-        anim.SetTrigger("Fade_Out");
-    }
-
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Game_scene");
     }
 
     public void QuitGame()
