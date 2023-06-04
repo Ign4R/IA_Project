cc0b714349efb95a3946a24c6b192275141a5ec4
Nacho <69459950+HereRyng@users.noreply.github.com>
2023-06-04 05:05:17 -0300

    move folder states base out folder IA

--- Assets/_Main/_Scripts/PlayerScripts/PlayerModel.cs
+++ Assets/_Main/_Scripts/PlayerScripts/PlayerModel.cs
@@ -13,9 +13,9 @@ public class PlayerModel : BaseModel
     {
       
         if (OnTakeDamage != null)
-        {
+        {         
             print("daño");
-            _rb.AddForce(Vector3.up * 200);
+            _rb.AddForce(Vector3.up * 200 + (-GetForward * 200));
             OnTakeDamage();
             lifes--;
         }
