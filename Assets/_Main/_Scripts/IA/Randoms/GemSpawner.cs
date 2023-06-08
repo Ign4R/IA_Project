d347fb8cfd79efeaeaef557ee08cab812a4e5136
Tomi <63859937+tomicancheff@users.noreply.github.com>
2023-06-08 15:07:02 -0300

    GemSpawner Script changes, flocking revision-
    few Changes for GemSpawner Script (  Now it works and manages to match the player score so it can spawn higher / lower values gems, but the gems spawns all at once in the scene, besides that the player score at the start will always be 0).
    New Sheeps prefabs and flocking scripts added
    Also added flocking manager component in my scene.

--- Assets/_Main/_Scripts/IA/Randoms/GemSpawner.cs
+++ Assets/_Main/_Scripts/IA/Randoms/GemSpawner.cs
@@ -5,37 +5,51 @@ using UnityEngine;
 
 public class GemSpawner : MonoBehaviour
 {
+    
     //VARIABLES DE GEMA, PREFABS, SCORES, WEIGHT, ETC.
     [SerializeField] private GameObject[] gemPrefabs;
     [SerializeField] private Transform[] gemSpawnPoints;
-    [SerializeField] private int numberOfGemsToSpawn = 3;
+    [SerializeField] private int numberOfGemsToSpawn;
     [SerializeField] private float gemWeightMultiplier = 1.0f;
-
     private List<Transform> availableSpawnPoints;
 
+    //SPAWN EN INTERVALO DE GEMAS.
+    // private float gemSpawnInterval = 5.0f; 
+    // private float timeSinceLastGemSpawn = 0.0f;
     private void Start()
     {
         InitializeSpawnPoints();
         SpawnGems();
     }
-
+    
+    //SPAWN INTERVALO DE GEMAS
+    // private void Update()
+    // {
+    //     timeSinceLastGemSpawn += Time.deltaTime;
+    //
+    //     if (timeSinceLastGemSpawn >= gemSpawnInterval)
+    //     {
+    //         timeSinceLastGemSpawn = 0.0f;
+    //         SpawnGems();
+    //     }
+    // }
     private void InitializeSpawnPoints()
     {
         availableSpawnPoints = new List<Transform>(gemSpawnPoints);
     }
-
-    //DICCIONARIO DE GEMAS, UTILIZACIÓN DE LISTA.
+    
+    //DICCIONARIO DE GEMAS
     private void SpawnGems()
     {
-        if (numberOfGemsToSpawn > availableSpawnPoints.Count)
-        {
-            Debug.LogWarning("No hay suficientes puntos de spawn disponibles para spawnear todas las gemas.");
-            return;
-        }
+        int gemsToSpawn = Mathf.Min(numberOfGemsToSpawn, availableSpawnPoints.Count);
+    
+        // Get player's current score from GameManager
+        int playerScore = GameManager.Instance.GetScore();
+    
+        // Pass in player's current score when building gem dictionary
+        var gemDict = BuildGemDictionary(playerScore);
 
-        var gemDict = BuildGemDictionary();
-
-        for (int i = 0; i < numberOfGemsToSpawn; i++)
+        for (int i = 0; i < gemsToSpawn; i++)
         {
             if (gemDict.Count > 0)
             {
@@ -48,16 +62,64 @@ public class GemSpawner : MonoBehaviour
             }
         }
     }
-
+    
+    //SPAWN EN INTERVALO DE GEMAS (REQUIERE EL UPDATE)
+    // private void SpawnGems()
+    // {
+    //     if (numberOfGemsToSpawn > availableSpawnPoints.Count)
+    //     {
+    //         Debug.LogWarning("No hay suficientes puntos de spawn disponibles para spawnear todas las gemas.");
+    //         return;
+    //     }
+    //
+    //     // Get player's current score from GameManager
+    //     int playerScore = GameManager.Instance.GetScore();
+    //
+    //     // Pass in player's current score when building gem dictionary
+    //     var gemDict = BuildGemDictionary(playerScore);
+    //
+    //     for (int i = 0; i < numberOfGemsToSpawn; i++)
+    //     {
+    //         if (gemDict.Count > 0)
+    //         {
+    //             SpawnGem(gemDict);
+    //         }
+    //         else
+    //         {
+    //             Debug.LogWarning("No se encontraron gemas válidas para spawnear.");
+    //             break;
+    //         }
+    //     }
+    // }
+    
     //MODIFICAR LOS GEMSCORES PARA QUE ESTÉN SOLO EN EL SCRIPT ITEM.
-    private Dictionary<GameObject, float> BuildGemDictionary()
+    private Dictionary<GameObject, float> BuildGemDictionary(int playerScore)
     {
         Dictionary<GameObject, float> gemDict = new Dictionary<GameObject, float>();
-        
+    
         for (int j = 0; j < gemPrefabs.Length; j++)
         {
             Item item = gemPrefabs[j].GetComponent<Item>();
             float adjustedGemScore = item.Score * gemWeightMultiplier;
+        
+            // Adjust gem scores based on player's current score
+            if (playerScore >= 100)
+            {
+                // Decrementa la probabilidad de spawnear gemas de alto valor.
+                if (adjustedGemScore >= 20)
+                {
+                    adjustedGemScore *= 0.5f;
+                }
+            }
+            else
+            {
+                // Incrementa la probabilidad de spawnear gemas de alto valor
+                if (adjustedGemScore >= 20)
+                {
+                    adjustedGemScore *= 2.0f;
+                }
+            }
+        
             gemDict[gemPrefabs[j]] = adjustedGemScore;
         }
         return gemDict;
@@ -70,7 +132,11 @@ public class GemSpawner : MonoBehaviour
         int randomIndex = Random.Range(0, availableSpawnPoints.Count);
         Transform spawnPoint = availableSpawnPoints[randomIndex];
         availableSpawnPoints.RemoveAt(randomIndex);
-        
+    
+        // Debugea el valor de la gema spawneada ( para testeo)
+        Item item = selectedGem.GetComponent<Item>();
+        Debug.Log("Spawned gem with value: " + item.Score);
+    
         Instantiate(selectedGem, spawnPoint.position, selectedGem.transform.rotation);
     }
 }
