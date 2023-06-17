223dad1750a36be9a6d45c668a05257fb77d72f0
Tomi <63859937+tomicancheff@users.noreply.github.com>
2023-06-17 15:42:08 -0300

    Few Flocking Set up / Settings ( tomi test scene)

--- Assets/_Main/_Members/Tomi/Sheep.cs
+++ Assets/_Main/_Members/Tomi/Sheep.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Sheep : MonoBehaviour
 {
+     
    //Lógica de movimiento y llamado de comportamiento de Flocking de las ovejas( similar al Batman script de la clase de Sebas)
    //Amoldar según movimiento de las mismas para que persigan a  la araña o arañas.
     
