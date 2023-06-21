44d8254763499b77623ccf3be5f92971b0080cfe
Tomi <63859937+tomicancheff@users.noreply.github.com>
2023-06-21 15:25:53 -0300

    NEW Flocking bugs Fixed
    ADDED new Win Panel
    ADDED Lose Panel buttons (and Win panel too)
    
    FIXED FLocking behaviour with sheeps, but needs rework.
    
    DELETED Unused Tomi old Scene.

--- Assets/_Main/_Members/Tomi/Sheep.cs
+++ Assets/_Main/_Members/Tomi/Sheep.cs
@@ -2,19 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sheep : MonoBehaviour
+public class Sheep : MonoBehaviour, IBoid, IFlocking
 {
-   //Lógica de movimiento y llamado de comportamiento de Flocking de las ovejas( similar al Batman script de la clase de Sebas)
-   //Amoldar según movimiento de las mismas en la escena.
-   
    public float speed;
    public float rotSpeed;
    public float radius;
-   Rigidbody _rb;
+   private Rigidbody _rb;
+
    private void Awake()
    {
       _rb = GetComponent<Rigidbody>();
    }
+
    public Vector3 Position => transform.position;
 
    public Vector3 Front => transform.forward;
@@ -27,17 +26,32 @@ public class Sheep : MonoBehaviour
       dir.y = _rb.velocity.y;
       _rb.velocity = dir;
    }
+
    public void LookDir(Vector3 dir)
    {
       dir.y = 0;
       transform.forward = Vector3.Lerp(transform.forward, dir, rotSpeed * Time.deltaTime);
    }
+
    private void OnDrawGizmosSelected()
    {
       Gizmos.color = Color.blue;
       Gizmos.DrawWireSphere(Position, radius);
    }
-  
-    
-    
+
+   public Vector3 GetDir(List<IBoid> boids, IBoid self)
+   {
+      // Implementa el comportamiento de flocking específico de las ovejas aquí
+      // y devuelve la dirección resultante.
+      // Puedes usar la lista de boids cercanos y los parámetros del self para calcular la dirección.
+
+      // Ejemplo de comportamiento: mantenerse cerca de los boids vecinos
+      Vector3 dir = Vector3.zero;
+      foreach (var boid in boids)
+      {
+         Vector3 toBoid = boid.Position - self.Position;
+         dir += toBoid;
+      }
+      return dir.normalized;
+   }
 }
